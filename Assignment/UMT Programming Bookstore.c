530a4b4ccfd97d29cb95e5fde90b8bb9a42f5470
RamlyBurger <112510782+RamlyBurger@users.noreply.github.com>
2022-09-03 22:39:00 +0800

    Add basic book menu display

--- Assignment/UMT Programming Bookstore.c
+++ Assignment/UMT Programming Bookstore.c
@@ -20,7 +20,19 @@ int main(void)
 
     if (option[0] == 1)
     {
-        printf("Book menu will be added later.\n");
+        printf("\nBook Menu\n");
+        printf("Software Development Programming\n");
+        printf("A. Introduction to Java Programming - RM69.00\n");
+        printf("B. Learn to Program with C (2015) - RM70.00\n");
+        printf("C. Java For Dummies - RM55.50\n");
+        printf("\nWeb Programming\n");
+        printf("D. Introduction to Web Programming - RM58.00\n");
+        printf("E. Introduction HTML & CSS - RM65.00\n");
+        printf("F. Introduction to PHP - RM89.00\n");
+        printf("\nMobile Programming\n");
+        printf("G. Mobile Design and Programming - RM73.50\n");
+        printf("H. Start Building Mobile App - RM80.00\n");
+        printf("I. Android Programming - RM50.00\n");
     }
     else if (option[0] == 2)
     {
