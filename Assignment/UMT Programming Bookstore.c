61477ba077de9439fa29ecb08334b9948da67377
RamlyBurger <112510782+RamlyBurger@users.noreply.github.com>
2022-09-03 22:05:00 +0800

    Show main menu options

--- Assignment/UMT Programming Bookstore.c
+++ Assignment/UMT Programming Bookstore.c
@@ -6,6 +6,10 @@ int main(void)
     printf("       UMT Programming Bookstore\n");
     printf("========================================\n");
     printf("A simple console POS system.\n");
+    printf("\n");
+    printf("1. View Menu\n");
+    printf("2. Sales Order\n");
+    printf("3. Exit\n");
 
     return 0;
 }
