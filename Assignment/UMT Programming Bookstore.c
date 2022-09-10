0359eb0383988c12a1eedb913b645176aee0da76
RamlyBurger <112510782+RamlyBurger@users.noreply.github.com>
2022-09-10 14:24:00 +0800

    Frame book list rows

--- Assignment/UMT Programming Bookstore.c
+++ Assignment/UMT Programming Bookstore.c
@@ -360,28 +360,28 @@ void menu()
 void software()
 {
     printf("\nSoftware Development Programming\n");
-    printf("Type                                   Price(RM)\n");
-    printf("A > Introduction to Java Programming      69.00\n");
-    printf("B > Learn to Program with C (2015)        70.00\n");
-    printf("C > Java For Dummies                      55.50\n");
+    printf("| Type                                   Price(RM) |\n");
+    printf("| A > Introduction to Java Programming      69.00 |\n");
+    printf("| B > Learn to Program with C (2015)        70.00 |\n");
+    printf("| C > Java For Dummies                      55.50 |\n");
 }
 
 void web()
 {
     printf("\nWeb Programming\n");
-    printf("Type                                   Price(RM)\n");
-    printf("D > Introduction to Web Programming       58.00\n");
-    printf("E > Introduction HTML & CSS               65.00\n");
-    printf("F > Introduction to PHP                   89.00\n");
+    printf("| Type                                   Price(RM) |\n");
+    printf("| D > Introduction to Web Programming       58.00 |\n");
+    printf("| E > Introduction HTML & CSS               65.00 |\n");
+    printf("| F > Introduction to PHP                   89.00 |\n");
 }
 
 void mobile()
 {
     printf("\nMobile Programming\n");
-    printf("Type                                   Price(RM)\n");
-    printf("G > Mobile Design and Programming         73.50\n");
-    printf("H > Start Building Mobile App             80.00\n");
-    printf("I > Android Programming                   50.00\n");
+    printf("| Type                                   Price(RM) |\n");
+    printf("| G > Mobile Design and Programming         73.50 |\n");
+    printf("| H > Start Building Mobile App             80.00 |\n");
+    printf("| I > Android Programming                   50.00 |\n");
 }
 
 void error()
