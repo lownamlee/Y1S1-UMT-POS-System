21bed01ee07302eefc58b764e0397f2882b7cef8
RamlyBurger <112510782+RamlyBurger@users.noreply.github.com>
2022-09-11 16:17:00 +0800

    Align order confirmation cursor

--- Assignment/UMT Programming Bookstore.c
+++ Assignment/UMT Programming Bookstore.c
@@ -223,7 +223,8 @@ void input(int qty[])
 
         if (index[0] == 'X')
         {
-            printf("Confirm the order? (Y = Yes, N = No): ");
+            printf("| Confirm the order? (Y = Yes, N = No):            |");
+            cursorMove(12);
             rewind(stdin);
             gets(ans);
             ans[0] = toupper(ans[0]);
@@ -237,7 +238,8 @@ void input(int qty[])
                     ans[i] = 0;
                 }
 
-                printf("Confirm the order? (Y = Yes, N = No): ");
+                printf("| Confirm the order? (Y = Yes, N = No):            |");
+                cursorMove(12);
                 rewind(stdin);
                 gets(ans);
                 ans[0] = toupper(ans[0]);
