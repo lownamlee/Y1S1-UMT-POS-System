9db0073a42d3196587e63b44fff33179ebe4ac75
RamlyBurger <112510782+RamlyBurger@users.noreply.github.com>
2022-09-15 19:17:00 +0800

    Align report total row

--- Assignment/UMT Programming Bookstore.c
+++ Assignment/UMT Programming Bookstore.c
@@ -369,8 +369,8 @@ void report(int sales, int totalQty[])
         printf("| %c | %14d | %8.2lf                       |\n", i + 65, totalQty[i], totalQty[i] * BOOK_PRICE[i]);
     }
 
-    box(1);
-    printf("| TOTAL | %10d | %8.2lf                    |\n", totalBook, totalPrice);
+    printf("|                                   =====   ========= |\n");
+    printf("| TOTAL                            %5d    %8.2lf |\n", totalBook, totalPrice);
     footer();
 }
 
